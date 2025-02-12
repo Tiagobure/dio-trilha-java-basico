4774674fe17a7d56aa8a21182f0ca62b705ab4ce
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-12 17:13:12 -0300

    fix: exercicio completo

--- src/application/ContaTerminal.java
+++ src/application/ContaTerminal.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.print("Please enter you name: ");
+		String name = sc.nextLine(); 
+		
+		System.out.print("Please enter the account number: ");
+ 
+		int number = sc.nextInt();
+		sc.nextLine(); 
+		System.out.print("Please enter the Agency number: ");
+        String agency= sc.nextLine();
+		
+		System.out.print("your balance: ");
+		double balance = sc.nextDouble();
+		
+		
+		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, "
+				+ "sua agência é %s, conta %d e "
+				+ "seu saldo %.2f já está disponível para saque"
+				,name,agency,number,balance);
+		
 
 	}
 
